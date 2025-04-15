a79a95a6d5b9ae0011efff1b4e2baf0aaf376786
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-04-15 12:49:30 +0200

    base

---
+++ DependencyAnalyzerLib/src/main/java/org/example/DependencyAnalyserLib.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.concurrent.Future;
+
+public final class DependencyAnalyserLib {
+
+    public static Future<ClassDepsReport> getClassDependencies(java.nio.file.Path path){
+        return null;
+    }
+
+    public static Future<PackageDepsReport> getPackageDependencies(java.nio.file.Path path){
+        return null;
+    }
+
+    public static Future<ProjectDepsReport> getProjectDependencies(java.nio.file.Path path){
+        return null;
+    }
+}
