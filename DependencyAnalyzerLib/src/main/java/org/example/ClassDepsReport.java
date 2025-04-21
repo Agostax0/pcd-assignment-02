bcf3965bd0bac6b6e2945ebc139a755beddea264
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-04-21 17:59:41 +0200

    class deps report done

--- DependencyAnalyzerLib/src/main/java/org/example/ClassDepsReport.java
+++ DependencyAnalyzerLib/src/main/java/org/example/ClassDepsReport.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import static org.example.TreeBuilder.*;
+
 public class ClassDepsReport {
+    private final TreeGraph treeGraph;
+
+    public ClassDepsReport(final TreeGraph treeGraph){
+        this.treeGraph = treeGraph;
+    }
+    public TreeGraph getTreeGraph() {
+        return this.treeGraph;
+    }
+
+
 }
