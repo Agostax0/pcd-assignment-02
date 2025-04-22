98c7bd48b62b50fcf990b004586de82f60ca7c4a
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-04-22 12:27:05 +0200

    ProjectDepsReportTest done

---
+++ DependencyAnalyzerLib/src/test/java/ProjectDepsReportTest.java
@@ -0,0 +1,72 @@
+import com.github.javaparser.StaticJavaParser;
+import org.example.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ProjectDepsReportTest {
+    final Path srcPath = Paths.get("D:\\pcd\\DependencyAnalyzerLib\\src\\main\\resources\\");
+
+
+    private ProjectDepsReport projectDepsReport;
+    private TreeBuilder.TreeGraph tree;
+
+    private List<List<String>> importRefs;
+
+    private void initRefs(Path path) throws FileNotFoundException {
+
+
+        Arrays.stream(Objects.requireNonNull(path.toFile().listFiles())).forEach((file -> {
+            if(file.isDirectory()){
+                try {
+                    initRefs(file.toPath());
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            else{
+                ImportVisitor importVisitor = new ImportVisitor();
+                var imports = new ArrayList<ImportRef>();
+                try {
+                    importVisitor.visit(StaticJavaParser.parse(file), imports);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+                this.importRefs = imports.stream().map(ImportRef::getPackageTreeOfImport).toList();
+
+                var fileTree = new TreeBuilder.TreeGraph();
+
+                importRefs.forEach(fileTree::addConnections);
+
+                this.tree.addTree(fileTree);
+            }
+        }));
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        this.tree = new TreeBuilder.TreeGraph();
+        this.projectDepsReport = new ProjectDepsReport(this.tree);
+    }
+
+    @Test
+    void testContainsNodesFromDifferentFolders() throws FileNotFoundException {
+        initRefs(srcPath);
+
+        System.out.println(this.projectDepsReport.getGraph().getNodes());
+
+        assertTrue(this.projectDepsReport.getGraph().hasNode("util") && this.projectDepsReport.getGraph().hasNode("Accessible"));
+    }
+
+}
