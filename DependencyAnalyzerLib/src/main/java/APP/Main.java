f297ceb61e466d45eaf4073ecd4bd029be1c9dcb
Agosta Alessandro <agosta.alessandro@studio.unibo.it>
2025-04-29 13:11:24 +0200

    new imports

--- DependencyAnalyzerLib/src/main/java/APP/Main.java
+++ DependencyAnalyzerLib/src/main/java/APP/Main.java
@@ -6,7 +6,7 @@ import javax.swing.AbstractAction;
 import com.github.javaparser.javadoc.description.JavadocDescriptionElement;
 import com.github.javaparser.javadoc.description.JavadocDescription;
 import com.github.javaparser.javadoc.description.JavadocSnippet;
-
+import com.github.javaparser.ast.expr.Name;
 public class Main {
     public static void main(String[] args) {
         new DependencyFrame();
